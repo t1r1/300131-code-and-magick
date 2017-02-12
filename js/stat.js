f6ec7578286eecff131624da824045f4360aca21
Maria <tirimaria@gmail.com>
2017-02-12 20:16:31 +0300

    encapsulated each module with an IIFE

--- js/stat.js
+++ js/stat.js
@@ -1,6 +1,6 @@
 'use strict';
 
-window.renderStatistics = function (ctx, names, times) {
+window.renderStatistics = (function (ctx, names, times) {
 
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
   ctx.fillRect(110, 20, 420, 270);
@@ -9,7 +9,6 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.strokeStyle = 'Black';
   ctx.fillStyle = '#000';
   ctx.font = '16px PT Mono';
-  // ctx.fillText('Ура, вы победили!', 120, 40);
   ctx.fillText('Список результатов:', 120, 55);
   var max = -1;
   var min = Infinity;
@@ -46,4 +45,4 @@ window.renderStatistics = function (ctx, names, times) {
     ctx.fillText(name, histoX + columnIndent * i, 100 + histoHeight + 20);
     ctx.fillText(time.toFixed(0), histoX + columnIndent * i, 230 - height);
   }
-};
+})();
